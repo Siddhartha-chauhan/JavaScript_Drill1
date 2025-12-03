1ee947a250bb8ede0a632f9e1e760319382a629e
Siddhartha Chauhan <siddharthachauhan.connect@gmail.com>
2025-12-03 17:33:25 +0530

    Add comments for Problem 6 functionality
    Added comments to clarify the purpose of the function.

--- problems/problem6.js
+++ problems/problem6.js
@@ -1,3 +1,6 @@
+// Problem 6: Get only BMW and Audi cars from the inventory
+// Expected output: Array of BMW and Audi car objects (use JSON.stringify to view)
+
 import { inventory } from '../inventory.js';
 
 function problem6(inventory) {
