c720f8fc44cc676ba70cc2bcad587f14b2f2f92f
Siddharthachauhan <siddharthachauhan.connect@gmail.com>
2025-12-03 16:59:17 +0530

    Solved Problem 6

---
+++ problems/problem6.js
@@ -0,0 +1,15 @@
+import { inventory } from '../inventory.js';
+
+function problem6(inventory) {
+    let BMWAndAudi = [];
+
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
+            BMWAndAudi.push(inventory[i]);
+        }
+    }
+
+    return BMWAndAudi;
+}
+
+export { problem6 };
---
+++ test/test6.js
@@ -0,0 +1,6 @@
+import { problem6 } from "../problems/problem6.js";
+import { inventory } from "../inventory.js";
+
+let result = problem6(inventory);
+
+console.log(JSON.stringify(result));
